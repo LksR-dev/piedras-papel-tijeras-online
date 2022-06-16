fd98369dda86ba6baf41b17a42922cc891f5717e
Lucas <lucasmruiz05@gmail.com>
2022-06-16 19:39:46 -0300

    add app universal

--- server/index.ts
+++ server/index.ts
@@ -257,6 +257,9 @@ app.put("/rooms/:id/player/move", (req, res) => {
 const relativeRoute = path.resolve(__dirname, "../dist");
 
 app.use(express.static(relativeRoute));
+app.get("*", (req, res) => {
+  res.sendFile(relativeRoute);
+});
 
 app.listen(port, () => {
   console.log(`listen the port: ${port}`);
