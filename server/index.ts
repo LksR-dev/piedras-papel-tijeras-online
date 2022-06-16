637815a7f9ab9e84cc1b6344a725a82c58ba54ab
Lucas <lucasmruiz05@gmail.com>
2022-06-16 10:57:06 -0300

    problemas para deployar parte millon

--- server/index.ts
+++ server/index.ts
@@ -258,9 +258,9 @@ app.put("/rooms/:id/player/move", (req, res) => {
 const relativeRoute = path.resolve(__dirname, "../dist");
 
 app.use(express.static(relativeRoute));
-// app.get("*", (req, res) => {
-//   res.sendFile(relativeRoute, +"/index.html");
-// });
+app.get("*", (req, res) => {
+  res.sendFile(relativeRoute + "/index.html");
+});
 
 app.listen(port, () => {
   console.log(`listen the port: ${port}`);
