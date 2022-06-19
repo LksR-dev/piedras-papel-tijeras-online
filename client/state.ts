1218d9b57d44277f5c2d02447c7964defaa50601
Lucas <lucasmruiz05@gmail.com>
2022-06-19 19:39:19 -0300

    change api base url

--- client/state.ts
+++ client/state.ts
@@ -1,7 +1,7 @@
 import { rtdb } from "./rtdb";
 
-// const API_BASE_URL = "https://piedra-papel-tijeras-online-df.herokuapp.com";
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "https://piedra-papel-tijeras-online-df.herokuapp.com";
+// const API_BASE_URL = "http://localhost:3000";
 
 const state = {
   data: {
